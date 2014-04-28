5151a20e0a36e4fac556d07f18b96d3556576be0
James Wang <veriathon@gmail.com>
2014-04-28 00:46:23 -0700

    Fully functional site, including webform

--- README.md
+++ README.md
@@ -5,6 +5,16 @@ WonderCal prototype website
 
 =========
 
+##Run Instructions:
+
+- Install Python environment
+- Make sure you have pip (Python package installer)
+- pip install requirements.txt
+- `python wondercal.py`
+
+After you run the first three steps once, you can always just run `python wondercal.py` directly in the future.
+
+
 Landing Page:
 
 Version A: index.html
--- wondercal.py
+++ wondercal.py
@@ -7,19 +7,52 @@
     :copyright: (c) 2014 by TimePickle Team
 
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def landing_page():
-    return render_template('index.html')
+    if request.method == 'POST':
+        return redirect('/thanks')
+    else:
+        return render_template('index.html')
 
 
 @app.route('/2')
-def ab_landing():
+def ab_landing_page():
     return render_template('index-b.html')
+
+
+@app.route('/team')
+def team_page():
+    return render_template('our-team.html')
+
+
+@app.route('/team2')
+def ab_team_page():
+    return render_template('our-team-b.html')
+
+
+@app.route('/faq')
+def faq_page():
+    return render_template('faq.html')
+
+
+@app.route('/faq2')
+def ab_faq_page():
+    return render_template('faq-b.html')
+
+
+@app.route('/beta')
+def beta_page():
+    return render_template('beta.html')
+
+
+@app.route('/thanks')
+def email_confirm_page():
+    return render_template('email-confirm.html')
 
 
 if __name__ == '__main__':
